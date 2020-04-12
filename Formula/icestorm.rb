b6974429d6f31484c7c7f5543207d9029f544712
Donn <12652988+donn@users.noreply.github.com>
2020-04-12 15:34:39 +0200

    Removed sha256, incremented version

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -2,9 +2,8 @@ class Icestorm < Formula
     desc "Reverse-engineered Lattice iCE40 FPGAs Bitstream Documentation"
     homepage "http://www.clifford.at/icestorm/"
     url "https://github.com/cliffordwolf/icestorm/archive/master.tar.gz"
-    sha256 "b0537fb173e6af20f88b0bc2ed0a75467662ba04af3096012c07c59c84afe09d"
     head "https://github.com/cliffordwolf/icestorm"
-    version "0.1.0"
+    version "0.2.0"
 
     depends_on "libftdi"
     depends_on "yosys"
